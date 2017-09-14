37dae2f45b3639a41bc3fa34ef33830c229ae8b4
ferrerasousa19.88@gmail.com <65528803>
2017-09-14 16:23:42 -0300

    Add Arquivos

---
+++ index.php
@@ -0,0 +1,268 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!-- saved from url=(0026)http://www.brasilevoli.it/ -->
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+	<meta http-equiv="Content-Language" content="it">
+
+	<title>Volicharter</title>
+	<meta name="keywords" content="air italy brasile, blu panorama brasile, volo charter brasile, volo di linea tap, voli di linea tam, voli di linea iberia, voli di linea air europa, fly brasil, voe brasil, todo brasil, volo fortaleza, volo natal, volo salvador, hotel in brasile, albergo in brasile, investimenti in brasile, case in brasile, acquista in brasile, voli per il brasile, tour in brasile, tour in buggy, passeio buggy natal, pousadas in brasile, appartamenti in brasile, appartamenti a fortaleza, appartamenti a natal, soggiorni in brasile, pacchetti brasile, pacchetti vacanza, offerte brasile, offerte viaggi, viaggi e soggiorno a fortaleza, pontanegra, praia de iracema, copacabana, ipanema, sao paolo, manaus, salvador de bahia, natal, recife, maceio, porto seguro, belo horizonte, porto allegre, pipa, curitiba, morro do sao paulo, fernando de noronha, jericoacoara, rio de janeiro, amazzonia, pantanal, carioca, agenzia viaggi, tour operator, informazione brasile, viaggi in brasile, tour del brasile, canoa quebrada, voli low cost brasile, alberghi in brasile, brasile last minute, voli last minute, voli low cost,  voli last second, brasile vacanze, brasile viaggi, brasile volo, cuba, cuba hotel, cuba last minute, cuba last second, cuba vacanza, cuba viaggi, cuba voli, compagnia aerea brasile, coppa del mondo calcio, copa do mundo 2014, mondiali di calcio, olimpiadi rio 2016">
+	<meta name="description" content=""> 
+	<meta name="keyphrase" content="">
+	<link rel="STYLESHEET" 
+		  type="text/css" 
+		  href="./assets/css/Stile.css">
+	<script type="text/javascript" 
+	async="" 
+	src="./Brasile Voli Tour Operator - Gli Specialisti del Brasile_files/ga.js.transferir">
+	</script>
+	
+	<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-4630443-8']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+<style type="text/css">
+.style1 {
+	font-size: xx-small;
+}
+</style>
+<script 
+	src="./Brasile Voli Tour Operator - Gli Specialisti del Brasile_files/jquery.min.js.transferir" 
+	type="text/javascript">
+</script>
+<script src="./Brasile Voli Tour Operator - Gli Specialisti del Brasile_files/fadeslideshow.js.transferir" 
+		type="text/javascript">
+</script>
+<script type="text/javascript">
+
+var mygallery=new fadeSlideShow({
+	wrapperid: "fadeshow1", //ID of blank DIV on page to house Slideshow
+	dimensions: [728, 286], //width/height of gallery in pixels. Should reflect dimensions of largest image
+	imagearray: [
+		["/images/banner1.jpg"],
+		["/images/banner2.jpg"],
+		["/images/banner3.jpg"],
+		["/images/banner4.jpg"], 
+		["/images/banner5.jpg"],
+		["/images/banner6.jpg"],
+		["/images/banner7.jpg"],
+		["/images/banner8.jpg"],
+		["/images/banner9.jpg"],
+		["/images/banner10.jpg"], 
+		["/images/banner11.jpg"],
+		["/images/banner12.jpg"],
+		["/images/banner13.jpg"],
+		["/images/banner14.jpg"], 
+		["/images/banner15.jpg"],
+		["/images/banner16.jpg"],
+		["/images/banner17.jpg"],
+		["/images/banner18.jpg"],
+		["/images/banner19.jpg"],
+		["/images/banner20.jpg"]
+	],
+	displaymode: {type:'auto', pause:3000, cycles:0, wraparound:false},
+	persist: false, //remember last viewed slide and recall within same session?
+	fadeduration: 1000, //transition duration (milliseconds)
+	descreveal: "ondemand",
+	togglerid: ""
+})
+
+var mygallery=new fadeSlideShow({
+	wrapperid: "fadeshow2", //ID of blank DIV on page to house Slideshow
+	dimensions: [200, 90], //width/height of gallery in pixels. Should reflect dimensions of largest image
+	imagearray: [
+		["/images/logo2.gif"],
+		["/images/logo3.gif"]
+	],
+	displaymode: {type:'auto', pause:2500, cycles:0, wraparound:false},
+	persist: false, //remember last viewed slide and recall within same session?
+	fadeduration: 800, //transition duration (milliseconds)
+	descreveal: "ondemand",
+	togglerid: ""
+})
+
+</script></head>
+
+
+
+
+
+<body id="0">
+
+    
+    <div id="top">	
+		<div id="topContentSx">
+		<img src="./Brasile Voli Tour Operator - Gli Specialisti del Brasile_files/logo.png"><br>
+		
+		<div class="offerta">
+		<a href="http://www.brasilevoli.it/default.asp?offerta=S"><div class="titolo_offerta">OFFERTISSIMA milano fortaleza</div></a>
+		<a href="http://www.brasilevoli.it/default.asp?offerta=S"><div class="sottotitolo_offerta">volo AIR ITALY</div></a>
+		<a href="http://www.brasilevoli.it/default.asp?offerta=S"><div class="titolo_link">partenza 10 o 17 luglio eur 670 andata e ritorno</div></a>
+		</div>
+		
+		</div>
+	</div>
+	
+	<div id="main" class="autoclr">
+		<div id="sottomenu">
+		  <a href="http://www.brasilevoli.it/default.asp"><img src="./Brasile Voli Tour Operator - Gli Specialisti del Brasile_files/homepage_on.gif"></a> 
+		  <a href="http://www.brasilevoli.it/mediagallery.asp"><img src="./Brasile Voli Tour Operator - Gli Specialisti del Brasile_files/scopri_off.gif"></a>
+		</div>
+		<div id="bannerSx">
+			<div id="fadeshow1" style="position: relative; visibility: visible; background: white; overflow: hidden; width: 728px; height: 286px;"><div class="gallerylayer" style="position: absolute; left: 0px; top: 0px; width: 100%; height: 100%; background: white; z-index: 1000; opacity: 0.989928;"><img src="./Brasile Voli Tour Operator - Gli Specialisti del Brasile_files/banner1.jpg" style="border-width: 0px; display: none;">
+<img src="./Brasile Voli Tour Operator - Gli Specialisti del Brasile_files/banner2.jpg" style="border-width: 0px; display: none; margin-left: 0px; margin-top: 0px;">
+<img src="./Brasile Voli Tour Operator - Gli Specialisti del Brasile_files/banner3.jpg" style="border-width: 0px; display: none;">
+<img src="./Brasile Voli Tour Operator - Gli Specialisti del Brasile_files/banner4.jpg" style="border-width: 0px; display: none; margin-left: 0px; margin-top: 0px;">
+<img src="./Brasile Voli Tour Operator - Gli Specialisti del Brasile_files/banner5.jpg" style="border-width: 0px; display: none;">
+<img src="./Brasile Voli Tour Operator - Gli Specialisti del Brasile_files/banner6.jpg" style="border-width: 0px; display: none; margin-left: 0px; margin-top: 0px;">
+<img src="./Brasile Voli Tour Operator - Gli Specialisti del Brasile_files/banner7.jpg" style="border-width: 0px; display: none;">
+<img src="./Brasile Voli Tour Operator - Gli Specialisti del Brasile_files/banner8.jpg" style="border-width: 0px; display: none; margin-left: 0px; margin-top: 0px;">
+<img src="./Brasile Voli Tour Operator - Gli Specialisti del Brasile_files/banner9.jpg" style="border-width: 0px; display: none;">
+<img src="./Brasile Voli Tour Operator - Gli Specialisti del Brasile_files/banner10.jpg" style="border-width: 0px; display: none; margin-left: 0px; margin-top: 0px;">
+<img src="./Brasile Voli Tour Operator - Gli Specialisti del Brasile_files/banner11.jpg" style="border-width: 0px; display: none;">
+<img src="./Brasile Voli Tour Operator - Gli Specialisti del Brasile_files/banner12.jpg" style="border-width: 0px; display: none; margin-left: 0px; margin-top: 0px;">
+<img src="./Brasile Voli Tour Operator - Gli Specialisti del Brasile_files/banner13.jpg" style="border-width: 0px; display: none;">
+<img src="./Brasile Voli Tour Operator - Gli Specialisti del Brasile_files/banner14.jpg" style="border-width: 0px; display: none; margin-left: 0px; margin-top: 0px;">
+<img src="./Brasile Voli Tour Operator - Gli Specialisti del Brasile_files/banner15.jpg" style="border-width: 0px; display: none;">
+<img src="./Brasile Voli Tour Operator - Gli Specialisti del Brasile_files/banner16.jpg" style="border-width: 0px; display: block; margin-left: 0px; margin-top: 0px;">
+<img src="./Brasile Voli Tour Operator - Gli Specialisti del Brasile_files/banner17.jpg" style="border-width: 0px; display: none;">
+<img src="./Brasile Voli Tour Operator - Gli Specialisti del Brasile_files/banner18.jpg" style="border-width: 0px; display: none;">
+<img src="./Brasile Voli Tour Operator - Gli Specialisti del Brasile_files/banner19.jpg" style="border-width: 0px; display: none;">
+<img src="./Brasile Voli Tour Operator - Gli Specialisti del Brasile_files/banner20.jpg" style="border-width: 0px; display: none;">
+</div><div class="gallerylayer" style="position: absolute; left: 0px; top: 0px; width: 100%; height: 100%; background: white; z-index: 999; opacity: 1;"><img src="./Brasile Voli Tour Operator - Gli Specialisti del Brasile_files/banner1.jpg" style="border-width: 0px; display: none; margin-left: 0px; margin-top: 0px;">
+<img src="./Brasile Voli Tour Operator - Gli Specialisti del Brasile_files/banner2.jpg" style="border-width: 0px; display: none;">
+<img src="./Brasile Voli Tour Operator - Gli Specialisti del Brasile_files/banner3.jpg" style="border-width: 0px; display: none; margin-left: 0px; margin-top: 0px;">
+<img src="./Brasile Voli Tour Operator - Gli Specialisti del Brasile_files/banner4.jpg" style="border-width: 0px; display: none;">
+<img src="./Brasile Voli Tour Operator - Gli Specialisti del Brasile_files/banner5.jpg" style="border-width: 0px; display: none; margin-left: 0px; margin-top: 0px;">
+<img src="./Brasile Voli Tour Operator - Gli Specialisti del Brasile_files/banner6.jpg" style="border-width: 0px; display: none;">
+<img src="./Brasile Voli Tour Operator - Gli Specialisti del Brasile_files/banner7.jpg" style="border-width: 0px; display: none; margin-left: 0px; margin-top: 0px;">
+<img src="./Brasile Voli Tour Operator - Gli Specialisti del Brasile_files/banner8.jpg" style="border-width: 0px; display: none;">
+<img src="./Brasile Voli Tour Operator - Gli Specialisti del Brasile_files/banner9.jpg" style="border-width: 0px; display: none; margin-left: 0px; margin-top: 0px;">
+<img src="./Brasile Voli Tour Operator - Gli Specialisti del Brasile_files/banner10.jpg" style="border-width: 0px; display: none;">
+<img src="./Brasile Voli Tour Operator - Gli Specialisti del Brasile_files/banner11.jpg" style="border-width: 0px; display: none; margin-left: 0px; margin-top: 0px;">
+<img src="./Brasile Voli Tour Operator - Gli Specialisti del Brasile_files/banner12.jpg" style="border-width: 0px; display: none;">
+<img src="./Brasile Voli Tour Operator - Gli Specialisti del Brasile_files/banner13.jpg" style="border-width: 0px; display: none; margin-left: 0px; margin-top: 0px;">
+<img src="./Brasile Voli Tour Operator - Gli Specialisti del Brasile_files/banner14.jpg" style="border-width: 0px; display: none;">
+<img src="./Brasile Voli Tour Operator - Gli Specialisti del Brasile_files/banner15.jpg" style="border-width: 0px; display: block; margin-left: 0px; margin-top: 0px;">
+<img src="./Brasile Voli Tour Operator - Gli Specialisti del Brasile_files/banner16.jpg" style="border-width: 0px; display: none;">
+<img src="./Brasile Voli Tour Operator - Gli Specialisti del Brasile_files/banner17.jpg" style="border-width: 0px; display: none;">
+<img src="./Brasile Voli Tour Operator - Gli Specialisti del Brasile_files/banner18.jpg" style="border-width: 0px; display: none;">
+<img src="./Brasile Voli Tour Operator - Gli Specialisti del Brasile_files/banner19.jpg" style="border-width: 0px; display: none;">
+<img src="./Brasile Voli Tour Operator - Gli Specialisti del Brasile_files/banner20.jpg" style="border-width: 0px; display: none;">
+</div><img src="./Brasile Voli Tour Operator - Gli Specialisti del Brasile_files/loading.gif" style="position: absolute; left: 337px; top: 88px; display: none;"></div>	
+		</div>
+		<div id="bannerDx">
+			<div class="testomax_bianco">telefoni: <br>
+				0735431134 | 0735783264</div><div class="testo_bianco">dalle 10.00 alle 13.45 dalle 16.00 alle 20.45</div>	
+			<div class="testomax_bianco">cellulari: <span class="style1">dalle 09.30 alle 24.00</span><br>
+				3921988658 | 3383787775</div>
+			<div class="inner_dx"><a class="bigW" href="mailto:volibrasile@gmail.com">volibrasile@gmail.com</a></div>
+			<div class="inner_dx2">
+			<a href="https://www.facebook.com/#!/profile.php?id=100001703890263" title="Facebook" target="_blank"><img alt="Collegamento a Facebook" src="./Brasile Voli Tour Operator - Gli Specialisti del Brasile_files/seguici_facebook.png"></a></div>
+			<div class="inner_dx2">
+			<!--
+			Skype 'Skype Me�!' button
+			http://www.skype.com/go/skypebuttons
+			-->
+			<script type="text/javascript" src="./Brasile Voli Tour Operator - Gli Specialisti del Brasile_files/skypeCheck.js.transferir"></script>
+			<a href="skype:volibrasile?call"><img src="./Brasile Voli Tour Operator - Gli Specialisti del Brasile_files/skype.png" style="border: none;" width="124" height="52" alt="Skype Me�!"></a>			
+			</div>
+		</div>		
+		
+	</div>
+	<div id="main" class="autoclr">	
+		<div id="mainContent">
+		  <div id="mainTop">
+		  
+		  
+					<form method="POST" action="http://www.brasilevoli.it/default.asp?cerca=true" name="form_ricerca">
+					<table class="standardFormTable" border="0" style="width: 916px; height: 50px;">
+						<tbody><tr>
+					    	<td class="formSx" align="right">Cerca destinazione</td>
+					    	<td></td>
+					    	<td style="width: 768px">
+					    	<select name="destinazione" class="EditSelect">
+					    		<option value="tutte">tutte</option>
+					    		
+					    		  <option value="FORTALEZA SOLO ANDATA">fortaleza solo andata</option>
+					    		  
+					    		  <option value="FORTALEZA SOLO RITORNO">fortaleza solo ritorno</option>
+					    		  
+					    	</select>
+					    	</td>
+					    	<td rowspan="3" style="width: 215px">
+					    	&nbsp;</td>
+					    	<td class="formDx" rowspan="3">
+					    	<input class="EditButton" type="submit" value="avvia ricerca" name="Button_login"></td>
+					    	<td rowspan="3">
+							<div id="fadeshow2" style="position: relative; visibility: visible; background: white; overflow: hidden; width: 200px; height: 90px;"><div class="gallerylayer" style="position: absolute; left: 0px; top: 0px; width: 100%; height: 100%; background: white; z-index: 999; opacity: 1;"><img src="./Brasile Voli Tour Operator - Gli Specialisti del Brasile_files/logo2.gif" style="border-width: 0px; display: none;">
+<img src="./Brasile Voli Tour Operator - Gli Specialisti del Brasile_files/logo3.gif" style="border-width: 0px; display: block; margin-left: 0px; margin-top: 0px;">
+</div><div class="gallerylayer" style="position: absolute; left: 0px; top: 0px; width: 100%; height: 100%; background: white; z-index: 1000; opacity: 0.843702;"><img src="./Brasile Voli Tour Operator - Gli Specialisti del Brasile_files/logo2.gif" style="border-width: 0px; display: block; margin-left: 0px; margin-top: 0px;">
+<img src="./Brasile Voli Tour Operator - Gli Specialisti del Brasile_files/logo3.gif" style="border-width: 0px; display: none;">
+</div><img src="./Brasile Voli Tour Operator - Gli Specialisti del Brasile_files/loading.gif" style="position: absolute; left: 73px; top: -10px; display: none;"></div>					    	
+					    	</td>
+					    </tr>
+					    <tr>
+					    	<td class="formSx" align="right">Aeroporto di 
+							partenza</td>
+					    	<td class="formMx"></td>
+					    	<td class="formDx" style="width: 768px">
+					    	<select name="partenza" class="EditSelect">
+					    		<option value="tutti">tutti</option>
+					    		
+					    		  <option value="FORTALEZA">fortaleza</option>
+					    		  
+					    		  <option value="MILANO MXP">milano mxp</option>
+					    		  					    	</select>
+					    	</td>
+					    </tr>
+					    <tr>
+					    	<td class="formSx" align="right">&nbsp;</td>
+					    	<td class="formMx">&nbsp;</td>
+					    	<td class="formDx" style="width: 768px">
+					    	&nbsp;</td>
+					    </tr>
+					    </tbody></table>
+					</form>		  
+		  
+		  </div>
+		  <div id="mainElenco">
+			<table class="elenco" cellpadding="8" cellspacing="0">
+				<tbody><tr>
+					<td width="32%"><span class="darkbold">DESTINAZIONE</span></td>
+					<td width="14%"><span class="darkbold">DATA PART.</span></td>
+					<td width="18%"><span class="darkbold">DA/PER</span></td>
+					<td width="14%"><span class="darkbold">DURATA</span></td>
+					<td width="10%"><span class="darkbold">PREZZO</span></td>
+					<td width="6%"><span class="darkbold"> DETTAGLI</span></td>
+					<td width="6%"><span class="darkbold"> PRENOTA</span></td>					
+				</tr>
+								
+			</tbody></table>
+		  </div>	
+		</div>
+	
+	</div>
+    
+    <div id="bottom">	
+
+			<div id="mainContentSx">
+			<br><span class="testomini">� 2011 All rights reserved <strong>BRASILE VOLI</strong><br>
+			ORGANIZZAZIONE: ASTRO VIAGGI SRL via piemonte,25 SAN BENEDETTO DEL TRONTO - p.iva 01893770444 - <strong>codice licenza IATA 3827612</strong> </span></div>
+				<div id="mainContentSxSx">
+				<img alt="" height="64" src="./Brasile Voli Tour Operator - Gli Specialisti del Brasile_files/iata.png" width="95" class="iata"></div>
+			<div id="mainContentDxDx">
+				<a href="https://www.facebook.com/#!/profile.php?id=100001703890263" class="social" title="Facebook" target="_blank"><img alt="facebook" src="./Brasile Voli Tour Operator - Gli Specialisti del Brasile_files/facebook.png" class="dxdx"></a></div>			
+			    
+	</div>	
+
+<div id="skypedetectioncontainer" style="position: absolute; width: 5px; height: 5px; top: 0px; left: -10px; visibility: visible;"></div></body></html>
